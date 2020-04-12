943169df0a15a62f93d48a0a241f5500d034e754
ozguraysenur <54063783+ozguraysenur@users.noreply.github.com>
2020-04-13 00:35:47 +0200

    completed

---
+++ assignment-04/src/Assignment4/Controller.java
@@ -0,0 +1,53 @@
+package Assignment4;
+
+import com.sun.org.apache.bcel.internal.generic.MONITORENTER;
+
+import javax.jws.WebParam;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/controller")
+public class Controller extends HttpServlet {
+
+    private final Logic _logic = new Logic();
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        serviceReq(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        serviceReq(req,resp);
+    }
+
+    private void serviceReq (HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+
+        String first=req.getParameter("FirstNumber");
+        String second= req.getParameter("SecondNumber");
+
+        Model model=_logic.add(first,second);
+
+        // pass model to view (we can either pass attributes or session)
+        req.setAttribute("Model",model);
+        HttpSession session = req.getSession();
+        session.setAttribute("Model", model);
+
+        ServletContext context = getServletContext();
+        RequestDispatcher dispatcher = context.getRequestDispatcher("/view");
+
+        // pass modified request and response to view
+        //forward modified req to another servlet
+        dispatcher.forward(req, resp);
+
+    }
+}
---
+++ assignment-04/src/Assignment4/Model.java
@@ -0,0 +1,25 @@
+package Assignment4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+
+    List<Integer> results= new ArrayList<>();
+
+    public final int sum ;
+
+    public Model(int _sum){
+        sum=_sum;
+        results.add(sum);
+    }
+
+    public List<Integer> getList(){
+        return results;
+    }
+
+    public int getSum(){
+        return sum;
+    }
+
+}
